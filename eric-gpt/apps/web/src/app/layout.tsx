ab8d2c349f2e0920f0bfd9b5643332454ae77a9d
Collins Lekakeny <lekakenycollins8@gmail.com>
2025-05-21 19:11:41 +0300

    chore: add environment configs and suppress hydration warning in layout

--- eric-gpt/apps/web/src/app/layout.tsx
+++ eric-gpt/apps/web/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default async function RootLayout({
   const session = await getServerSession(authOptions);
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Providers session={session}>
           {children}
