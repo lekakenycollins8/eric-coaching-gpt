a22916ad84137742e3a6d14c3fb7294ab89c2ccb
Collins Lekakeny <lekakenycollins8@gmail.com>
2025-06-16 16:14:19 +0300

    chore: update Next.js config to use serverExternalPackages instead of experimental flag

--- eric-gpt/apps/server/next.config.js
+++ eric-gpt/apps/server/next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Disable React Strict Mode to prevent warnings from Swagger UI components
-  swcMinify: true,
-  experimental: {
-    serverComponentsExternalPackages: ['swagger-ui-react'],
-  },
+  serverExternalPackages: ['swagger-ui-react'],
 };
 
 export default nextConfig;
